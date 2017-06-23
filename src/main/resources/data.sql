0df6dbb9701e467ae92bb2e5b387276fcd1a6411
MarieSugawara <tukimisyuu@gmail.com>
2017-06-23 16:13:44 +0900

    修正確認用コードの削除

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,7 +3,3 @@
 insert into schedule (dogtype, title) values 
     (0, '朝ごはん'),(0, '夜ごはん')
     ,(1, '朝ごはん'),(1, '夜ごはん');
-    
-
-insert into schedule (dogtype, title, day) values
-    (0, '日の出', '2017-06-23');
