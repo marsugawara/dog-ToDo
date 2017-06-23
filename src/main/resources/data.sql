757eb78b8f79ec8f9eb33d001d6d004b37965fb1
MarieSugawara <tukimisyuu@gmail.com>
2017-06-23 14:42:16 +0900

    文字化け確認

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,7 @@
 insert into schedule (dogtype, title) values 
     (0, '朝ごはん'),(0, '夜ごはん')
     ,(1, '朝ごはん'),(1, '夜ごはん');
+    
+
+insert into schedule (dogtype, title, day) values
+    (0, '日の出', '2017-06-23');
